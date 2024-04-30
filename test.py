ad9062180f7f0f5d7315a9fdda129a7071880394
nurisrahmid <nurisrahmid@gmail.com>
2024-04-30 19:14:49 +0800

    First Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#menyambungkan nama
+#first_name = 'Nur'
+#last_name = 'israhmid'
+#full_name = f'{first_name}{last_name}'
+#print(full_name)
+
+#mengatur waktu
+from datetime import datetime
+
+today = datetime.now()
+
+date_time  = today.strftime('%Y-%m-%d-%H-%M-%S')
+print(date_time)
+
